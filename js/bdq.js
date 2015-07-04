3ced18e426c12749c458f3cf071243f2e85e2262
Jani Anderson <ja@janianderson.com>
2015-07-03 23:36:58 -0500

    Scaffolding Setup
    Nav HTML & Bootstrap setup
    BDQ JS object
    Datepicker install

---
+++ js/bdq.js
@@ -0,0 +1,14 @@
+var BDQ = {
+    "init"  :   function() {
+        $('.input-group.date').datetimepicker();       
+    },
+    "showMainNav" : function() {},
+    "hideMainNav" : function() {},
+    "showForm" : function() {},
+    "hideForm" : function() {}
+
+}
+
+$(function(){
+    BDQ.init();
+});
