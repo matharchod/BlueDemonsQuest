c6cc0ff523de537cc4721a069597d996327c81d3
Jani Anderson <ja@janianderson.com>
2015-07-09 13:13:10 -0500

    Sync Re-try

--- js/bdq.js
+++ js/bdq.js
@@ -1,14 +1,68 @@
 var BDQ = {
     "init"  :   function() {
-        $('.input-group.date').datetimepicker();       
+        // Datepicker plugin init
+        $('.input-group.date').datetimepicker();
     },
-    "showMainNav" : function() {},
+    "showMainNav" : function() {alert()},
     "hideMainNav" : function() {},
     "showForm" : function() {},
-    "hideForm" : function() {}
+    "hideForm" : function() {},
+    "updateIFrame"  : function(content) {
+        $('#D_Frame').contents(content);
+        console.log(content);
+    },
+    "requestCrossDomain": function ( site, callback ) {            
+        // Accepts a url and a callback function to run.
+        // If no url was passed, exit.
+        if ( !site ) {
+            alert('No site was passed.');
+            return false;
+        }
+         
+        // Take the provided url, and add it to a YQL query. Make sure you encode it!
+        var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=cbFunc';
+        
+        console.log('yql = ', yql);
+         
+        // Request that YSQL string, and run a callback function.
+        // Pass a defined function to prevent cache-busting.
+        $.getJSON( yql, cbFunc );
+         
+        function cbFunc(data) {
+        // If we have something to work with...
+        if ( data.results[0] ) {
+            
+            // Strip out all script tags, for security reasons.
+            // BE VERY CAREFUL. This helps, but we should do more. 
+            data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
+             
+            // If the user passed a callback, and it
+            // is a function, call it, and send through the data var.
+            if ( typeof callback === 'function') {
+                callback(data);
+            }
+            
+
+        }
+        // Else, Maybe we requested a site that doesn't exist, and nothing returned.
+        else throw new Error('Nothing returned from getJSON.');
+        
+    }
+}
+ 
 
 }
 
+
 $(function(){
     BDQ.init();
+
+    //EVENT LISTENERS FOR MAIN NAV       
+    $('a').click(function(){
+        BDQ.requestCrossDomain('http://www.cnn.com', function(results){
+//             $('#D_Frame').contents(results);
+console.log(results);
+        });
+    });
+    
 });
